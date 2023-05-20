e008b4c4cb9e677fbb3320bba631743bb8010d25
nikolaDrljaca <devnikoladr@gmail.com>
2023-05-20 15:20:48 +0300

    bfs and dfs on graphs

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,44 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+import Queue from "./Queue";
 
+export default function bfs(
+  graph: WeightedAdjacencyMatrix,
+  source: number,
+  needle: number,
+): number[] | null {
+  const seen = new Array(graph.length).fill(false);
+  const prev = new Array(graph.length).fill(-1);
+  seen[source] = true;
+  const queue: number[] = [source];
+
+  while (queue.length) {
+    const curr = queue.shift() as number;
+    if (curr === needle) {
+      break;
+    }
+    const adj = graph[curr];
+    for (let i = 0; i < adj.length; i++) {
+      if (adj[i] === 0) {
+        continue;
+      }
+      if (seen[i]) {
+        continue;
+      }
+      seen[i] = true;
+      prev[i] = curr;
+      queue.push(i);
+    }
+  }
+  if (prev[needle] === -1) {
+    return null;
+  }
+  //build it backwards
+  let curr = needle;
+  const out: number[] = [];
+
+  while (prev[curr] !== -1) {
+    out.push(curr);
+    curr = prev[curr];
+  }
+
+  return [source].concat(out.reverse());
 }
--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,42 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(
+  graph: WeightedAdjacencyList,
+  curr: number,
+  needle: number,
+  seen: boolean[],
+  path: number[],
+): boolean {
+  //we've seen this and not found it, go elsewhere
+  if (seen[curr]) return false;
+  //pre
+  seen[curr] = true;
+  path.push(curr);
+  if (curr === needle) {
+    return true;
+  }
+  //recurse, for each vertex in adjacency list
+  const adj = graph[curr];
 
+  for (let i = 0; i < adj.length; i++) {
+    const edge = adj[i];
+    if (walk(graph, edge.to, needle, seen, path)) {
+      return true;
+    }
+  }
+
+  //post
+  path.pop();
+  return false;
+}
+
+export default function dfs(
+  graph: WeightedAdjacencyList,
+  source: number,
+  needle: number,
+): number[] | null {
+  const seen = new Array(graph.length).fill(false);
+  const path: number[] = [];
+
+  walk(graph, source, needle, seen, path);
+  if (path.length === 0) return null;
+  return path;
 }
